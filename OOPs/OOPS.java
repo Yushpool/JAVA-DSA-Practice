509c791bef3f3c89e952dc34f9b27cda9666e651
Ayush Bhosale <bhosaleayush45@gmail.com>
2025-06-25 17:13:08 +0530

    copy constructors

---
+++ OOPs/OOPS.java
@@ -0,0 +1,37 @@
+// import java.util.*;
+
+class Student{
+    String name;
+    int age;
+
+    public void printInfo(){
+        System.out.println(this.age);
+        System.out.println(this.name);
+    }
+
+    public Student(Student s2){
+        this.age = s2.age;
+        this.name = s2.name;
+    }
+
+    Student(){
+
+    }
+
+    // public Student(Student s1) {
+    //     //TODO Auto-generated constructor stub
+    // }
+}
+
+public class OOPS {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.age = 21;
+        s1.name = "Ayush";
+        Student s2 = new Student(s1);
+        s2.printInfo();
+        
+    }
+
+    
+}
