73976dfa4cb4e3f50ccff99839679f3f013c4f06
Ayush Bhosale <bhosaleayush45@gmail.com>
2025-07-15 15:18:17 +0530

    Sum of n Natural nums by recursion

---
+++ Recursion/BasicQ/printSumOfN.java
@@ -0,0 +1,21 @@
+package BasicQ;
+
+public class printSumOfN {
+
+    public static int sumOfN(int n){
+        if(n == 1){
+            return 1;
+        }
+        return  n + sumOfN(n-1);
+
+}
+
+public static void main(String[] args) {
+    int n = 5;
+    System.out.println(sumOfN(n));
+    
+}
+
+
+    
+}
