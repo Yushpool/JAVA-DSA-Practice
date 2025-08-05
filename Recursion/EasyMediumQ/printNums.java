7be8944f3683a5bfa617f161f39d75a9bb73e49f
Ayush Bhosale <bhosaleayush45@gmail.com>
2025-08-05 23:33:20 +0530

    Recursion example - basic

---
+++ Recursion/EasyMediumQ/printNums.java
@@ -0,0 +1,22 @@
+//print numbers n to 1 (Decresing order)
+
+public class printNums {
+
+    public static int inRev(int n){
+
+        if(n == 1){
+            return 1;
+        }
+
+
+        System.out.print(n + " ");
+        return inRev(n-1);
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(inRev(n));
+    }
+
+    
+}
