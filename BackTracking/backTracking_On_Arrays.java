7fa36f2e8f8a4eeabdbb5965c6f011d101635e9a
Ayush Bhosale <bhosaleayush45@gmail.com>
2026-01-18 14:50:27 +0530

    Backtracking codes

---
+++ BackTracking/backTracking_On_Arrays.java
@@ -0,0 +1,37 @@
+
+
+public class backTracking_On_Arrays {
+
+    public static void changeArr(int arr[], int i, int val){
+
+        //base case
+        if(i == arr.length){
+            printArr(arr);
+            return;
+        }
+
+        arr[i] = val;
+        changeArr(arr, i+1, val+1);
+        arr[i] = arr[i] - 2;
+    }
+
+    public static void printArr(int arr[]){
+        System.out.print("[ ");
+        for(int i = 0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.print("]");
+        System.out.println();
+    }
+
+
+
+    public static void main(String[] args) {
+        int [] arr = new int[5];
+        changeArr(arr, 0, 1);
+        printArr(arr);
+        
+    }
+
+    
+}
