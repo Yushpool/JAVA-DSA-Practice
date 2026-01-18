1ff4684be8307424d33313dc287814b20da00e79
Ayush Bhosale <bhosaleayush45@gmail.com>
2026-01-18 14:46:49 +0530

    Subtree of Tree

---
+++ Binary Trees/substree.java
@@ -0,0 +1,63 @@
+public class substree {
+
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static boolean isIdentical(Node node, Node subRoot){
+        if(node == null && subRoot == null){
+            return true;
+        }
+        if(node == null || subRoot == null || node.data != subRoot.data){
+            return false;
+        }
+
+        if(!isIdentical(node.left, subRoot.left)){
+            return false;
+        }
+        if(!isIdentical(node.right, subRoot.right)){
+            return false;
+        }
+
+        return true;
+    }
+
+    public static boolean isSubtree(Node root, Node subRoot){
+        if(root == null){
+            return false;
+        }
+
+        if(root.data == subRoot.data){
+            if(isIdentical(root, subRoot)){
+                return true;
+            }
+        }
+
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+
+        Node subT = new Node(2);
+        subT.left = new Node(4);
+        subT.right = new Node(5);
+
+        System.out.println(isSubtree(root, subT));
+    }
+    
+}
