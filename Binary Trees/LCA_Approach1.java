780a8ddbe77810462b04fb79a39bfffadde47a32
Ayush Bhosale <bhosaleayush45@gmail.com>
2026-01-30 17:26:10 +0530

    Distance between two nodes

--- Binary Trees/LCA_Approach1.java
+++ Binary Trees/LCA_Approach1.java
@@ -58,22 +58,51 @@ public class LCA_Approach1 {
 
     }
 
-    public static Node lca2(Node root, int n1, int n2){
-        if(root == null || root.data == n1 || root.data == n2){
+    public static Node lca2(Node root, int n1, int n2) {
+        if (root == null || root.data == n1 || root.data == n2) {
             return root;
         }
 
-        Node leftLCA =lca2(root.left, n1, n2);
+        Node leftLCA = lca2(root.left, n1, n2);
         Node rigthLCA = lca2(root.right, n1, n2);
 
-        if(rigthLCA == null){
+        if (rigthLCA == null) {
             return leftLCA;
         }
-        if(leftLCA == null){
+        if (leftLCA == null) {
             return rigthLCA;
         }
 
         return root;
+    }
+
+    public static int localDist(Node root, int n) {
+        if (root == null) {
+            return -1;
+        }
+        if (root.data == n) {
+            return 0;
+        }
+
+        int leftDist = localDist(root.left, n);
+        int rightDist = localDist(root.right, n);
+
+        if (leftDist == -1 && rightDist == -1) {
+            return -1;
+        } else if (leftDist == -1) {
+            return rightDist + 1;
+        } else {
+            return leftDist + 1;
+        }
+    }
+
+    public static int minDist(Node root, int n1, int n2) {
+        Node lca = lca2(root, n1, n2);
+        int dist1 = localDist(lca, n1);
+        int dist2 = localDist(lca, n2);
+
+        return dist1 + dist2;
+
     }
 
     public static void main(String[] args) {
@@ -85,8 +114,9 @@ public class LCA_Approach1 {
         root.right.left = new Node(6);
         root.right.right = new Node(7);
 
-        int n1 = 4, n2 = 7;
+        int n1 = 4, n2 = 5;
         System.out.println(lca2(root, n1, n2).data);
+        System.out.println(minDist(root, n1, n2));
 
     }
 }
