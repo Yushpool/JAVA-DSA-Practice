2abaa84984a09451b0b9edec4b4a5d5d7a60a8e4
Ayush Bhosale <bhosaleayush45@gmail.com>
2026-04-06 12:26:45 +0530

    /solved print root to leaf paths

--- Binary Search Trees/BinarySearchTree.java
+++ Binary Search Trees/BinarySearchTree.java
@@ -98,6 +98,7 @@ public class BinarySearchTree {
         return root;
     }
 
+    //find all element in the range of k1 and k2
     public static void findInRange(Node root, int k1, int k2){
         
         if(root == null){
@@ -117,8 +118,32 @@ public class BinarySearchTree {
         }
     }
 
+    //Print all paths from from root to leaf
+    public static void printRoot2Leaf(Node root, ArrayList<Integer> path){
+
+        if(root == null){
+            return;
+        }
+
+        path.add(root.data);
+        if(root.left == null && root.right == null){
+            printPath(path);
+        }
+
+        printRoot2Leaf(root.left, path);
+        printRoot2Leaf(root.right, path);
+        path.remove(path.size()-1);
+    }
+
+    public static void printPath(ArrayList<Integer> path){
+        for(int i = 0; i < path.size(); i++){
+            System.out.print(path.get(i) + "->");
+        }
+        System.out.println("Null");
+    }
+
     public static void main(String[] args) {
-        int [] values = {5, 3,1,4,6,2};
+        int [] values = {8,5,3,1,4,6,10,11,14};
         Node root = null;
 
         for(int i = 0; i<values.length; i++){
@@ -138,6 +163,9 @@ public class BinarySearchTree {
         // inorder(root);
 
         findInRange(root, 3, 6);
+        System.out.println();
+
+        printRoot2Leaf(root, new ArrayList<>());
 
 
 
